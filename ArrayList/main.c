8ecd122c16f5fed58857c9af05fb0b764283c782
maejo-o <efforterjisulee@gmail.com>
2023-02-19 10:56:51 +0900

    Initial Commit

---
+++ ArrayList/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ArrayList
+//
+//  Created by 이지수 on 2023/02/19.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
