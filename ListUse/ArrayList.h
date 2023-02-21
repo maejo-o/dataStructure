2253aad9ad01a557de0e3a547281be694a565909
maejo-o <efforterjisulee@gmail.com>
2023-02-22 03:28:43 +0900

    Point

--- ListUse/ArrayList.h
+++ ListUse/ArrayList.h
@@ -1,12 +1,31 @@
-//
-//  ArrayList.h
-//  ArrayList
-//
-//  Created by 이지수 on 2023/02/21.
-//
+#ifndef __ARRAY_LIST_H__
+#define __ARRAY_LIST_H__
 
-#ifndef ArrayList_h
-#define ArrayList_h
+#define TRUE    1
+#define FALSE    0
+
+/*** ArrayList¿« ¡§¿« ****/
+#define LIST_LEN    100
+typedef int LData;
+
+typedef struct __ArrayList
+{
+    LData arr[LIST_LEN];
+    int numOfData;
+    int curPosition;
+} ArrayList;
 
 
-#endif /* ArrayList_h */
+/*** ArrayListøÕ ∞¸∑√µ» ø¨ªÍµÈ ****/
+typedef ArrayList List;
+
+void ListInit(List * plist);
+void LInsert(List * plist, LData data);
+
+int LFirst(List * plist, LData * pdata);
+int LNext(List * plist, LData * pdata);
+
+LData LRemove(List * plist);
+int LCount(List * plist);
+
+#endif
