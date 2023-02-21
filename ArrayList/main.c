fb4e3e1187da15370d08135c3bf4b51a2201ddac
maejo-o <efforterjisulee@gmail.com>
2023-02-21 14:45:48 +0900

    prob03-1

--- ArrayList/main.c
+++ ArrayList/main.c
@@ -6,12 +6,12 @@ int main(void)
     // ArrayList 의 생성 및 초기화 ///
     List list;
     int data;
+    int sum=0;
     ListInit(&list);
     
-    // 5 개의 데이터 저장
-    LInsert(&list, 11); LInsert(&list, 11);
-    LInsert(&list, 22); LInsert(&list, 22);
-    LInsert(&list, 33);
+    // 9 개의 데이터 저장
+    for (int i=1; i<10; i++)
+        LInsert(&list, i);
     
     // 저장된 데이터의 전체 출력
     printf("현재 데이터의 수 : %d\n ", LCount(&list));
@@ -25,15 +25,33 @@ int main(void)
     }
     printf("\n\n");
     
-
-    // 숫자 22를 탐색하여 모두 삭제
     if (LFirst(&list, &data))
     {
-        if (data==22)
+        sum+=data;
+    }
+    
+    printf("SUM = %d\n\n", sum);
+
+    // 2의 배수를 탐색하여 모두 삭제
+    if (LFirst(&list, &data))
+    {
+        if ((data%2)==0)
             LRemove(&list);
         while (LNext(&list, &data))
         {
-            if (data==22)
+            if ((data%2)==0)
+                LRemove(&list);
+        }
+    }
+    
+    // 3의 배수를 탐색하여 모두 삭제
+    if (LFirst(&list, &data))
+    {
+        if ((data%3)==0)
+            LRemove(&list);
+        while (LNext(&list, &data))
+        {
+            if ((data%3)==0)
                 LRemove(&list);
         }
     }
@@ -46,7 +64,7 @@ int main(void)
         printf("%d ", data);
         
         while (LNext(&list, &data))
-            printf("&d ", data);
+            printf("%d ", data);
     }
     printf("\n\n");
     return 0;
---
+++ ListUse/ArrayList.h
@@ -0,0 +1,12 @@
+//
+//  ArrayList.h
+//  ArrayList
+//
+//  Created by 이지수 on 2023/02/21.
+//
+
+#ifndef ArrayList_h
+#define ArrayList_h
+
+
+#endif /* ArrayList_h */
