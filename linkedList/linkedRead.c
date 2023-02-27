277d990c52c5f2c04c9e1eed322e80f3bcff2cec
maejo-o <efforterjisulee@gmail.com>
2023-02-27 22:58:59 +0900

    really got it LinkedList

--- linkedList/linkedRead.c
+++ linkedList/linkedRead.c
@@ -9,46 +9,45 @@ typedef struct _node
 
 int main(void)
 {
-    Node *head=NULL;
-    Node *tail=NULL;
-    Node *cur=NULL;
+    Node * head = NULL;
+    Node * tail = NULL;
+    Node * cur = NULL;
     
     Node * newNode = NULL;
     int readData;
     
-    // 데이터를 입력받는 과정
-    while (1)
+    // 데이터를 입력 받는 과정
+    while(1)
     {
-        printf("자연수 입력 : ");
+        printf("Input the data : ");
         scanf("%d", &readData);
-        if (readData<1)
+        
+        if(readData<1)
             break;
         
-        // 노드의 추가과정
+        // 노드의 추가 과정
         newNode = (Node*)malloc(sizeof(Node));
-        newNode->data = readData;
-        newNode->next = NULL;
+        newNode->data=readData;
+        newNode->next=NULL;
         
-        if (head == NULL)
-            head = newNode;
+        if(head==NULL)
+            head=newNode;
         else
-            tail->next = newNode;
-        tail = newNode;
+            tail->next=newNode;
+        tail=newNode;
     }
     printf("\n");
     
-    // 입력받은 데이터의 출력과정
-    printf("입력 받은 데이터의 전체 출력! \n");
-    if(head == NULL)
-    {
-        printf("저장된 자연수가 존재하지 않습니다. \n");
-    }
+    // 입력받은 데이터의 출력 과정
+    printf("Print all data!\n");
+    if(head==NULL)
+        printf("No data to print. \n");
     else
     {
-        cur = head;
+        cur=head;
         printf("%d ", cur->data);
         
-        while(cur->next != NULL)
+        while(cur->next!=NULL)
         {
             cur = cur->next;
             printf("%d ", cur->data);
@@ -58,23 +57,21 @@ int main(void)
     
     // 메모리의 해제과정
     if(head == NULL)
-    {
         return 0;
-    }
     else
     {
-        Node * delNode = head;
+        Node *delNode = head;
         Node *delNextNode = head->next;
         
-        printf("%d 을 (를) 삭제합니다. \n", head->data);
+        printf("delete the \"%d\".\n", head->data);
         free(delNode);
         
-        while(delNextNode != NULL)
+        while(delNextNode!=NULL)
         {
             delNode = delNextNode;
             delNextNode = delNextNode->next;
             
-            printf("%d 을(를) 삭제합니다. \n", delNode->data);
+            printf("delete the \"%d\".\n", delNode->data);
             free(delNode);
         }
     }
