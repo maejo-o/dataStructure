1561a588f365aea3f9fdc4a6bec81ce0728e9d74
maejo-o <efforterjisulee@gmail.com>
2023-05-22 22:15:36 +0900

    folderSize_c

--- CH07/folderSize.c
+++ CH07/folderSize.c
@@ -4,5 +4,24 @@
 //
 //  Created by 이지수 on 2023/05/22.
 //
-
+#include <stdio.h>
 #include "folderSize.h"
+
+// size 를 루트 노드의 데이터필드로 하여 왼쪽과 오른쪽 서브 트리를 연결하는 연산
+treeNode* makeRootNode(int size, treeNode* leftNode, treeNode* rightNode) {
+    treeNode* root = (treeNode*)malloc(sizeof(treeNode));
+    root->size = size;
+    root->left = leftNode;
+    root->right = rightNode;
+    return root;
+}
+
+// 각 폴더 크기를 계산하기 위한 후위 순회 연산
+int postorder_FolderSize(treeNode* root) {
+    if(root) {
+        postorder_FolderSize(root->left); // L
+        postorder_FolderSize(root->right); // R
+        FolderSize += root->size;
+    }
+    return FolderSize;
+}
