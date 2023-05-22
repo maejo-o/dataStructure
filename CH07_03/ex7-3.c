5a56b2b48551fa3f73ca3372296263e16588120f
maejo-o <efforterjisulee@gmail.com>
2023-05-23 00:40:28 +0900

    ex7_3

--- CH07_03/ex7-3.c
+++ CH07_03/ex7-3.c
@@ -5,10 +5,18 @@
 //  Created by 이지수 on 2023/05/22.
 //
 
-#include <iostream>
+#include <stdio.h>
+#include "threadBT.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main(void) {
+    // (A*B-C/D) 수식 이진 트리 만들기
+    treeThNode* n7 = makeRootThNode('D', NULL, NULL, 0);
+    treeThNode* n6 = makeRootThNode('C', NULL, NULL, 1);
+    treeThNode* n5 = makeRootThNode('B', NULL, NULL, 1);
+    treeThNode* n4 = makeRootThNode('A', NULL, NULL, 1);
+    
+    treeThNode* n3 = makeRootThNode('/', n6, n7, 0);
+    treeThNode* n2 = makeRootThNode('*', n4, n5, 0);
+    
+    treeThNode* n1 = makeRootThNode('-', n2, n3, 0);
 }
