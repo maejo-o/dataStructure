0882a129f2ef84a2b0ff105ab8ba4a1e572c1d17
maejo-o <efforterjisulee@gmail.com>
2023-05-23 20:31:37 +0900

    node.c

--- CH07_04/node.c
+++ CH07_04/node.c
@@ -4,5 +4,13 @@
 //
 //  Created by 이지수 on 2023/05/23.
 //
-
+#include <stdio.h>
 #include "node.h"
+// 이진 탐색 트리를 중위 순회하면서 출력하는 연산
+void displayInorder(treeNode* root) {
+    if(root) {
+        displayInorder(root->left);
+        printf("%c ", root->key);
+        displayInorder(root->right);
+    }
+}
