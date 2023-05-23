86261d332f9f8ca0c3379bed5d3da791fbed7987
maejo-o <efforterjisulee@gmail.com>
2023-05-23 20:27:55 +0900

    ex07-04

---
+++ CH07_04/node.c
@@ -0,0 +1,8 @@
+//
+//  node.c
+//  CH07_04
+//
+//  Created by 이지수 on 2023/05/23.
+//
+
+#include "node.h"
---
+++ CH07_04/node.h
@@ -0,0 +1,13 @@
+//
+//  node.h
+//  CH07_04
+//
+//  Created by 이지수 on 2023/05/23.
+//
+
+#ifndef node_h
+#define node_h
+
+#include <stdio.h>
+
+#endif /* node_h */
